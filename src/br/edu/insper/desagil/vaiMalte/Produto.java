af245573e962520a50ac81a9d3a0e5705fa887c6
BrunoFNRodrigues <bf17669@gmail.com>
2021-03-17 13:36:10 -0300

    class produto

--- src/br/edu/insper/desagil/vaiMalte/Produto.java
+++ src/br/edu/insper/desagil/vaiMalte/Produto.java
@@ -1,5 +1,24 @@
 package br.edu.insper.desagil.vaiMalte;
 
 public class Produto {
-
+	private int codigo;
+	private String nome;
+	private double preco;
+	
+	public int getCodigo() {
+		return codigo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public double getPreco() {
+		return preco;
+	}
+	public Produto(int codigo, String nome, double preco) {
+		super();
+		this.codigo = codigo;
+		this.nome = nome;
+		this.preco = preco;
+	}
+		
 }
