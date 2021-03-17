1e855b102a93c5675ae82355173b2f6f47b75e84
keiyanishio <keiyan@al.insper.edu.br>
2021-03-17 11:28:23 -0300

    Pedido

--- src/br/edu/insper/desagil/vaiMalte/Pedido.java
+++ src/br/edu/insper/desagil/vaiMalte/Pedido.java
@@ -1,5 +1,30 @@
 package br.edu.insper.desagil.vaiMalte;
 
 public class Pedido {
+	private int quantidade;
+	private Produto produto;
+	
+	public Pedido(Produto produto) {
+		super();
+		this.quantidade = 1;
+		this.produto = produto;
+	}
+	public int getQuantidade() {
+		return quantidade;
+	}
 
+	public Produto getProduto() {
+		return produto;
+	}
+	
+	public void incremento(Produto produto) {
+		if (Carrinho.getPedidos().contains(produto)) {
+			this.quantidade+=1;
+		}
+	}
+	
+	public double total(Produto produto) {
+		return this.quantidade*produto.getPreco();
+	}
+		
 }
