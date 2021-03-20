726f459296b7493c923da92e91abc091997697d7
keiyanishio <keiyan@al.insper.edu.br>
2021-03-19 21:10:15 -0300

    Teste E

--- src/br/edu/insper/desagil/vaiMalte/Testador.java
+++ src/br/edu/insper/desagil/vaiMalte/Testador.java
@@ -59,6 +59,19 @@ public class Testador {
 	}
 
 	public boolean testeE() {
+		int res;
+		Produto produto = new Produto(5, "Cevada", 8.00);
+		Produto produto2 = new Produto(6, "Malte", 12.20);
+		Carrinho carr = new Carrinho();
+		carr.carrinho(produto);
+		carr.carrinho(produto);
+		carr.carrinho(produto2);
+		Caixa caixa = new Caixa();
+		caixa.promocao(produto2, 10);
+		res = caixa.fazTotal(carr);
+		if (res == 26) {
+			return true;
+		}
 	    return false;
 	}
 	
